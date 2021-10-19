230a0935ab639f731774965805cae2514297b6ae
SamLegrand <sam.legrand@student.uantwerpen.be>
2021-10-19 17:18:31 +0200

    Implemented basic recommendation model (on australian dataset)

--- read_data.py
+++ read_data.py
@@ -4,6 +4,9 @@ from pandas.core.frame import DataFrame
 from tqdm import tqdm
 import ast
 import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.neighbors import NearestNeighbors
+import scipy
 
 
 #read file line-by-line and parse json, returns dataframe
@@ -29,10 +32,41 @@ def parse_json(filename_python_json:str, read_max:int=-1) -> pd.DataFrame:
 # df = parse_json('./data/steam_reviews.json')
 # print(df.head())
 
+def get_item_features():
+    items = parse_json('./data/steam_games.json')
+    items = items[['genres', 'tags', 'id','specs']]
+    items['id'].dropna(inplace=True)
+    # print(items['genres'].tolist())
+    # print(items)
+    # assert False
+    items['genres'] = items['genres'].fillna("").apply(set)
+    items['tags'] = items['tags'].fillna("").apply(set)
+    items['specs'] = items['genres'].fillna("").apply(set)
+    items['tags'] = items.apply(lambda x: list(set.union(x['genres'], x['tags'], x['specs'])), axis=1)
+    items = items.drop(['genres', 'specs'], axis=1)
+    mlb = MultiLabelBinarizer(sparse_output=True)
+    items = items.join(pd.DataFrame.sparse.from_spmatrix(mlb.fit_transform(items.pop('tags')), index=items.index, columns=['tag_' + c for c in mlb.classes_]))
+    return items
+
+items = get_item_features()
 df = parse_json('./data/australian_user_reviews.json')
 df = df.explode('reviews', ignore_index=True)
 
-
 df = pd.concat([df.drop(['reviews', 'user_url'], axis=1), pd.json_normalize(df.reviews)], axis=1).drop(['funny', 'helpful', 'posted', 'last_edited', 'review'], axis=1)
+df = df.groupby('user_id')['item_id'].apply(list).reset_index(name='item_id')
 print(df)
+X = scipy.sparse.csr_matrix(items.drop(['id'], axis=1).values)
+nbrs = NearestNeighbors(n_neighbors=10).fit(X)
 
+recommendation_list = []
+for index, row in tqdm(df.iterrows()):
+    reviewed_items = items[items['id'].isin(row['item_id'])]
+    if reviewed_items.empty:
+        recommendation_list.append([])
+        continue
+    user_vector = reviewed_items.drop(['id'], axis=1).mean()
+    nns = nbrs.kneighbors(scipy.sparse.csr_matrix(user_vector.values), 10, return_distance=False)[0]
+    recommendations = [items.loc[item]['id'] for item in nns]
+    recommendation_list.append(recommendations)
+
+df['recommendations'] = recommendation_list
