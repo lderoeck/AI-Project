61c0fdb806b11712ab6402a475639be7ac060d75
Sam Legrand <sam.legrand@student.uantwerpen.be>
2021-10-27 19:48:56 +0200

    Implement evaluation for different distance metrics

---
+++ evaluate_recommender.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from recommender import parse_json, ContentBasedRec
+
+def generate_gt():
+    gt = parse_json("./data/australian_users_items.json")
+    gt['items'] = gt['items'].apply(lambda items: [item['item_id'] for item in items])
+    gt = gt.drop(['user_url'], axis=1)
+    gt.to_parquet('./data/ground_truth.parquet')
+
+def evaluate(recommendations:pd.DataFrame):
+    eval = recommendations.drop(recommendations[~recommendations['recommendations'].astype(bool)].index)
+    gt = pd.read_parquet('./data/ground_truth.parquet')
+    eval = eval.merge(gt, on=['user_id'])
+
+    results_dict = dict()
+    # compute recall@k
+    eval['recommendations'] = eval['recommendations'].apply(set)
+    eval['items'] = eval['items'].apply(set)
+    eval['recall@k'] = eval.apply(lambda row: len(row['recommendations'].intersection(row['items']))/len(row['recommendations']), axis=1)
+    results_dict['recall@k'] = eval['recall@k'].mean()
+    return results_dict
+
+if __name__ == '__main__':
+    metrics = ['minkowski', 'chebyshev', 'cosine']
+    for metric in metrics:
+        rec = ContentBasedRec("./data/steam_games.json", sparse=False, distance_metric=metric)
+        rec.generate_recommendations("./data/australian_user_reviews.json")
+        print(evaluate(rec.recommendations))
