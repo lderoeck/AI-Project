0bcae5a29bd79bf973c5379e92d53c95d53a883c
Sam Legrand <sam.legrand@student.uantwerpen.be>
2021-10-28 15:30:52 +0200

    Implement tfidf + fix recall@k

--- evaluate_recommender.py
+++ evaluate_recommender.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from recommender import parse_json, ContentBasedRec
+import itertools
 
 def generate_gt():
     gt = parse_json("./data/australian_users_items.json")
@@ -16,13 +17,16 @@ def evaluate(recommendations:pd.DataFrame):
     # compute recall@k
     eval['recommendations'] = eval['recommendations'].apply(set)
     eval['items'] = eval['items'].apply(set)
-    eval['recall@k'] = eval.apply(lambda row: len(row['recommendations'].intersection(row['items']))/len(row['recommendations']), axis=1)
+    eval.drop(eval[~eval['items'].astype(bool)].index, inplace=True)
+    eval['recall@k'] = eval.apply(lambda row: len(row['recommendations'].intersection(row['items']))/len(row['items']), axis=1)
     results_dict['recall@k'] = eval['recall@k'].mean()
     return results_dict
 
 if __name__ == '__main__':
-    metrics = ['minkowski', 'chebyshev', 'cosine']
-    for metric in metrics:
-        rec = ContentBasedRec("./data/steam_games.json", sparse=False, distance_metric=metric)
+    metrics = ['cosine']
+    tfidf = [None, 'default', 'smooth', 'sublinear', 'smooth_sublinear']
+    combinations = list(itertools.product(metrics, tfidf))
+    for metric, tfidf in combinations:
+        rec = ContentBasedRec("./data/steam_games.json", sparse=False, distance_metric=metric, tfidf=tfidf)
         rec.generate_recommendations("./data/australian_user_reviews.json")
-        print(evaluate(rec.recommendations))
+        print(metric, tfidf, evaluate(rec.recommendations))
