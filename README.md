c3561eaa79011fd4e4b8b0cb5d9129ee779fb2da
lderoeck <32436336+lderoeck@users.noreply.github.com>
2021-11-02 17:39:26 +0100

    Adding documentation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AI-Project
 Recommender system for the AI Project course based on Content-based Recommender Systems 
+
+## Basic Recommender
+In the basic recommender we create a feature vector for each game, this vector consists out of the different tags a game is able to posses and is one-hot encoded. A user will be represented by a similar feature vector, composed of the median for their reviewed games. To recommend items to users we compute the distance from their feature vector to the feature vectors of the different games. 
+
+We utilized different distance metrics and experimented with different tf-idf approaches.
--- evaluate_recommender.py
+++ evaluate_recommender.py
@@ -5,38 +5,64 @@ import itertools
 from multiprocessing import Pool
 import os
 
-def generate_gt(target:str):
+
+def generate_gt(target: str) -> None:
+    """Generates ground truth data to target
+
+    Args:
+        target (str): Path to file destination
+    """
     gt = parse_json("./data/australian_users_items.json")
     gt['items'] = gt['items'].apply(lambda items: [item['item_id'] for item in items])
     gt = gt.drop(['user_url'], axis=1)
     gt.to_parquet(target)
 
-def evaluate(recommendations:pd.DataFrame):
-    eval = recommendations.drop(recommendations[~recommendations['recommendations'].astype(bool)].index)
+
+def evaluate(recommendations: pd.DataFrame) -> dict:
+    """Evaluate the recommendations based on ground truth
+
+    Args:
+        recommendations (pd.DataFrame): Dataframe consisting of user_id and their respective recommendations
+
+    Returns:
+        dict: a dict containing the recall@k and nDCG@k 
+    """
+    eval = recommendations.drop(recommendations[~recommendations['recommendations'].astype(bool)].index)  # drop all recommendations that are empty
     gt = pd.read_parquet('./data/ground_truth.parquet')
     eval = eval.merge(gt, on=['user_id'])
 
     results_dict = dict()
+    # drop all rows with no items (nothing to compare against)
     eval.drop(eval[~eval['items'].astype(bool)].index, inplace=True)
-    
+
     # compute nDCG@k
     eval['nDCG@k'] = eval.apply(lambda row: np.sum([(np.power(2, rec in row['items'])-1)/(np.log2(i+2)) for i, rec in enumerate(row['recommendations'])]), axis=1)
     eval['nDCG@k'] = eval.apply(lambda row: row['nDCG@k']/np.sum([1/(np.log2(i+2)) for i in range(len(row['recommendations']))]), axis=1)
     results_dict['nDCG@k'] = eval['nDCG@k'].mean()
-    
+
     # compute recall@k
     eval['items'] = eval['items'].apply(set)
     eval['recommendations'] = eval['recommendations'].apply(set)
     eval['recall@k'] = eval.apply(lambda row: len(row['recommendations'].intersection(row['items']))/len(row['items']), axis=1)
     results_dict['recall@k'] = eval['recall@k'].mean()
-    
+
     return results_dict
 
-def evaluate_recommender(metric, tfidf):
-    rec = ContentBasedRec("./data/steam_games.json", sparse=True, distance_metric=metric, tfidf=tfidf)
+
+def evaluate_recommender(metric: str, tfidf: str) -> tuple:
+    """Wrapper function to allow for easy evaluation of specific metrics and tf-idf methods
+
+    Args:
+        metric (str): distance metric to be used
+        tfidf (str): tf-idf method to be used
+
+    Returns:
+        tuple: (metric, tf-idf, evaluation)
+    """
+    rec = ContentBasedRec("./data/steam_games.json",sparse=True, distance_metric=metric, tfidf=tfidf)
     rec.generate_recommendations("./data/australian_user_reviews.json")
-    return evaluate(metric, tfidf, rec.recommendations)
-        
+    return (metric, tfidf, evaluate(rec.recommendations))
+
 
 if __name__ == '__main__':
     gt_file = './data/ground_truth.parquet'
